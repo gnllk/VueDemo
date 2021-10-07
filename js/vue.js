f93e4bf70490c06db29ecd45f55afb1b7173f17f
Leo <gnllk@qq.com>
2021-10-07 22:37:58 +0800

    first commit

---
+++ js/vue.js
@@ -0,0 +1,256 @@
+class Vue{
+    constructor(options){
+        this.$ele = options.el;
+        this.$data = options.data;
+        this.$methods = options.methods;
+        this.$computed = options.computed;
+        if(this.$ele){
+            this.$publisher = new Publisher(this);
+            this.createObjectProxy(options.data);
+            this.createObjectProxy(options.methods);
+            this.createObjectGetterProxy(options.computed);
+            new Compiler(this.$ele, this);
+            if(options.created && typeof(options.created) == 'function'){
+                options.created.call(this);
+            }
+        }
+    }
+    createObjectProxy(obj){
+        if(obj){
+            for(let key in obj){
+                Object.defineProperty(this, key, {
+                    get:()=>{
+                        return obj[key];
+                    },
+                    set:(newVal)=>{
+                        obj[key] = newVal;
+                    }
+                });
+            }
+        }
+    }
+    createObjectGetterProxy(obj){
+        if(obj){
+            for(let key in obj){
+                Object.defineProperty(this, key, {
+                    get:()=>{
+                        return obj[key];
+                    }
+                });
+            }
+        }
+    }
+}
+
+class Compiler{
+    constructor(ele, vue){
+        this.$ele = this.isElement(ele) ? ele : document.querySelector(ele);
+        this.$vue = vue;
+        let fragment = this.nodeToFragment(this.$ele);
+        this.compile(fragment, this.$vue.$data);
+        this.$ele.appendChild(fragment);
+    }
+    isElement(node){
+        return node.nodeType === 1;
+    }
+    isDirective(str){
+        return str.startsWith("v-");
+    }
+    isTextExpr(str){
+        return /\{\s*\{.+?\}\s*\}/gm.test(str);
+    }
+    nodeToFragment(node){
+        let fragment = document.createDocumentFragment();
+        let childNode;
+        while(childNode = node.firstChild){
+            fragment.appendChild(childNode);
+        }
+        return fragment;
+    }
+    compile(node){
+        node.childNodes.forEach(child=>{
+            if(this.isElement(child)){
+                this.compileElement(child);
+                this.compile(child);
+            } else {
+                this.compileText(child);
+            }
+        });
+    }
+    compileElement(node){
+        let attrs = node.attributes;
+        [...attrs].forEach(attr=>{
+            let {name, value} = attr;
+            if(this.isDirective(name)){
+                let [,directiveText] = name.split('-');
+                let [directive,content] = directiveText.split(':');
+                CompileUtils.update[directive](this.$vue, node, value, content);
+            }
+        });
+    }
+    compileText(node){ 
+        let textExpr = node.textContent;
+        if(this.isTextExpr(textExpr)){
+             CompileUtils.update['text'](this.$vue, node, textExpr);
+        }
+    }
+}
+
+CompileUtils = {
+    getExprValue: function(expr, data){
+        // get expr "user.name" value from data
+        return expr.split('.').reduce((prev, curr, i)=>{ 
+            return prev ? prev[curr] : null; 
+        }, data);
+    },
+    setExprValue: function(expr, data, value){
+        // set data property via expr
+        expr.split('.').reduce((prev, curr, i, arr)=>{
+            if(prev && i == arr.length - 1){
+                prev[curr] = value;
+            }
+            return prev ? prev[curr] : null; 
+        }, data);
+    },
+    getExprValueOrComputed: function(expr, vue){
+        let val = this.getExprValue(expr, vue.$data);
+        if(!val){
+            let fn = this.getExprValue(expr, vue.$computed);
+            return fn && typeof(fn) == 'function' ? fn.call(vue) : fn;
+        }
+        return val;
+    },
+    getExpr: function(exprWithBrace){
+        // will return user.name from {{user.name}}
+        return /\{\s*\{(.+?)\}\s*\}/.exec(exprWithBrace)[1];
+    },
+    compileTextExpr: function(textExpr, getExprValFn){
+        // compile text expr like "Name: {{user.name}}, Age: {{user.age}}"
+        return textExpr.replace(/\{\s*\{(.+?)\}\s*\}/gm, match=>{
+            return getExprValFn(this.getExpr(match));
+        });
+    },
+    update:{
+        text:function(vue, node, textExpr){
+            // update text expr {{user.name}} with expr value
+            node.textContent = CompileUtils.compileTextExpr(textExpr, expr1=>{
+                // new subscriber to watch property changed event
+                let sub = new Subscriber(expr1, newVal=>{
+                    node.textContent = CompileUtils.compileTextExpr(textExpr, expr2=>{
+                        return CompileUtils.getExprValueOrComputed(expr2, vue);
+                    });
+                });
+                vue.$publisher.addSub(sub);
+                return CompileUtils.getExprValueOrComputed(expr1, vue);
+            });
+        },
+        html:function(vue, node, expr){
+            let textExpr = '{{' + expr + '}}';
+            // update text expr {{user.name}} with expr value
+            node.innerHTML = CompileUtils.compileTextExpr(textExpr, expr1=>{
+                // new subscriber to watch property changed event
+                let sub = new Subscriber(expr1, newVal=>{
+                    node.innerHTML = CompileUtils.compileTextExpr(textExpr, expr2=>{
+                        return CompileUtils.getExprValueOrComputed(expr2, vue);
+                    });
+                });
+                vue.$publisher.addSub(sub);
+                return CompileUtils.getExprValueOrComputed(expr1, vue);
+            });
+        },
+        model: function(vue, node, expr){
+            // new subscriber to watch property changed event
+            let sub = new Subscriber(expr, newVal=>{
+                node.value = newVal;
+            });
+            vue.$publisher.addSub(sub);
+            // update input value with expr value
+            node.value = CompileUtils.getExprValue(expr, vue.$data);
+            // listen input event changed
+            node.addEventListener('input', ()=>{
+                CompileUtils.setExprValue(expr, vue.$data, node.value);
+            });
+        },
+        on: function(vue, node, expr, eventName){
+            let fn = CompileUtils.getExprValue(expr, vue.$methods);
+            node.addEventListener(eventName, e=>{
+                if(fn && typeof(fn) == 'function'){
+                    fn.call(vue, e);
+                }
+            });
+        },
+        bind: function(vue, node, expr, attrName){
+            let val = CompileUtils.getExprValue(expr, vue.$data);
+            node.setAttribute(attrName, val);
+        }
+    }
+}
+
+class Subscriber{
+    constructor(topic, onUpdateFn){
+        this.$topic = topic;
+        this.$onUpdateFn = onUpdateFn;
+    }
+    getTopic(){
+        return this.$topic;
+    }
+    onUpdate(message){
+        this.$onUpdateFn(message);
+    }
+}
+
+class Publisher{
+    constructor(vue){
+        this.$vue = vue;
+        this.__subs = {};
+        new Observer(vue.$data, (topic, newVal)=>{
+            let subs = this.__subs[topic];
+            if(subs){
+                subs.forEach(sub=> sub.onUpdate(newVal));
+            }
+            let starSubs = this.__subs['*'];
+            if(starSubs){
+                starSubs.forEach(sub=> sub.onUpdate(newVal));
+            }
+        });
+    }
+    addSub(subscriber, topic){
+        if(!topic){
+            topic = subscriber.getTopic();
+        }
+        if(!this.__subs[topic]){
+            this.__subs[topic] = [];
+        }
+        this.__subs[topic].push(subscriber);
+    }
+}
+
+class Observer{
+    constructor(data, onPropertyChangedFn){
+        this.$data = data;
+        this.$onPropertyChangedFn = onPropertyChangedFn;
+        this.observer(data);
+    }
+    observer(data, parentsKey){
+        if(!data || typeof(data) != 'object') return;
+        // foreach properties
+        for(let key in data){
+            let val = data[key];
+            let expr = parentsKey ? parentsKey + '.' + key : key;
+            // observer sub object
+            this.observer(val, expr);
+            // define property get and set
+            Object.defineProperty(data, key, {
+                get:()=>{
+                    return val;
+                },
+                set:(newVal)=>{
+                    if(newVal == val) return;
+                    val = newVal;
+                    this.observer(newVal, expr);
+                    this.$onPropertyChangedFn(expr, newVal);
+                }
+            });
+        }
+    }
+}
